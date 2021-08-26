51bec42d77514e86309ebd52536e138d34330289
jenpyle <jennypyle440@gmail.com>
2021-08-25 21:19:45 -0600

    Add AsyncStorage and Netinfo

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Platform, KeyboardAvoidingView, Image } from 'react-native';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
+import { View, Text, StyleSheet, Platform, KeyboardAvoidingView, Image, Button } from 'react-native';
 import { GiftedChat, InputToolbar, Day, Bubble, SystemMessage, Actions, Composer, Send } from 'react-native-gifted-chat';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import NetInfo from '@react-native-community/netinfo';
 import { LogBox } from 'react-native';
 LogBox.ignoreLogs(['Setting a timer']);
 
@@ -30,6 +32,8 @@ export function Chat(props) {
 	const [text, setText] = useState('');
 	const [messages, setMessages] = useState([]);
 	const [uid, setUid] = useState('');
+	const [online, setOnline] = useState(false);
+	const isMounted = useRef(false);
 	let name = props.route.params.name;
 	let theme = props.route.params.themeColor;
 	let textColor = props.route.params.textColor;
@@ -46,7 +50,7 @@ export function Chat(props) {
 		headerTintColor: textColor,
 	});
 
-	referenceMessageUser = null;
+	// referenceMessageUser = null;
 
 	const onCollectionUpdate = (querySnapshot) => {
 		const messages = [];
@@ -83,30 +87,85 @@ export function Chat(props) {
 		}
 	};
 
+	const getMessages = async () => {
+		let messages = '';
+		try {
+			console.log('in getMEssages');
+			//read the messages in storage using getItem method, if none then set messages to be empty
+			messages = (await AsyncStorage.getItem('messages')) || [];
+			//asyncStorage can only store strings, use JSON.parse to convert string back to object
+			setMessages(JSON.parse(messages));
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+
+	// Save messages
+	const saveMessages = async () => {
+		try {
+			await AsyncStorage.setItem('messages', JSON.stringify(messages));
+			console.log('Messages saved to asyncStorge', messages);
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+
+	// Delete messages
+	const deleteMessages = async () => {
+		try {
+			await AsyncStorage.removeItem('messages');
+			console.log('deleted all messages');
+			setMessages([]);
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+
 	useEffect(() => {
+		// getMessages();
 		let unsubscribeMessages;
 		let authUnsubscribe;
+		NetInfo.fetch().then((connection) => {
+			if (connection.isConnected) {
+				setOnline(true);
+				console.log('Online');
 
-		//first check whether the user is signed in. If they’re not, you need to create a new user
-		//authenticate users anonymously onAuthStateChanged is an observer called when user's sign-in state changes and returns an unsubscribe function
-		authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
-			if (!user) {
-				//calls the firebase auth service
-				await firebase.auth().signInAnonymously();
+				//first check whether the user is signed in. If they’re not, you need to create a new user
+				//authenticate users anonymously onAuthStateChanged is an observer called when user's sign-in state changes and returns an unsubscribe function
+				authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
+					if (!user) {
+						//calls the firebase auth service
+						await firebase.auth().signInAnonymously();
+					}
+					setUid(user.uid);
+					//listen for collection changes across documents that belong to the current user and create updated snapshot of the collection
+					unsubscribeMessages = referenceChatMessages.orderBy('createdAt', 'desc').onSnapshot(onCollectionUpdate);
+				});
+
+				//componentWillUnmount
+				return () => {
+					// stop listening to authentication
+					authUnsubscribe();
+					// stop listening for changes
+					unsubscribeMessages();
+				};
+			} else {
+				setOnline(false);
+				console.log('Offline');
+				getMessages();
 			}
-			setUid(user.uid);
 		});
-		//listen for collection changes across documents that belong to the current user and create updated snapshot of the collection
-		unsubscribeMessages = referenceChatMessages.orderBy('createdAt', 'desc').onSnapshot(onCollectionUpdate);
-
-		//componentWillUnmount
-		return () => {
-			// stop listening to authentication
-			authUnsubscribe();
-			// stop listening for changes
-			unsubscribeMessages();
-		};
 	}, []); // checks for changes in the values in this array
+
+	//updates the asyncStorage messages when new messages are added
+	useEffect(() => {
+		if (isMounted.current) {
+			console.log('Saving messages');
+			saveMessages();
+		}
+
+		isMounted.current = true;
+	}, [messages]);
 
 	const renderBubble = (props) => (
 		//create renderBubble function after adding the renderBubble prop in the GiftedChat component
@@ -141,18 +200,21 @@ export function Chat(props) {
 
 	const renderSystemMessage = (props) => <SystemMessage {...props} textStyle={{ color: systemTextColor, fontWeight: '900' }} />;
 
-	const renderInputToolbar = (props) => (
-		<InputToolbar
-			{...props}
-			containerStyle={{
-				backgroundColor: rightBubble,
-				paddingTop: 4,
-				borderTopWidth: 2,
-				borderTopColor: accentColor,
-			}}
-			primaryStyle={{ alignItems: 'center' }}
-		/>
-	);
+	const renderInputToolbar = (props) =>
+		online ? (
+			<InputToolbar
+				{...props}
+				containerStyle={{
+					backgroundColor: rightBubble,
+					paddingTop: 4,
+					borderTopWidth: 2,
+					borderTopColor: accentColor,
+				}}
+				primaryStyle={{ alignItems: 'center' }}
+			/>
+		) : (
+			false
+		);
 
 	const renderComposer = (props) => (
 		//the inner text input space
@@ -224,16 +286,17 @@ export function Chat(props) {
 
 	const renderDay = (props) => <Day {...props} textStyle={{ color: systemTextColor }} />;
 
-	const onSend = (newMessages = []) => {
+	// Use Callback hook prevent this method from be recreated every render.
+	const onSend = useCallback((newMessages = []) => {
 		setMessages((prevMessages) => GiftedChat.append(prevMessages, newMessages));
 		addMessage(newMessages[0]);
-	}; //parameter previousState, which is a reference to the component’s state at the time the change is applied
+		saveMessages();
+	}, []); //parameter previousState, which is a reference to the component’s state at the time the change is applied
 	//message a user has just sent gets appended to the state messages so that it can be displayed in the chat.
 
 	const styles = StyleSheet.create({
 		container: {
 			flex: 1,
-			// alignItems: 'center',!!BAD!
 			justifyContent: 'center',
 			backgroundColor: theme,
 		},
@@ -241,6 +304,7 @@ export function Chat(props) {
 
 	return (
 		<View style={styles.container}>
+			<Button onPress={deleteMessages} title="Delete all test messages" color="#FF0000" accessibilityLabel="Learn more about this purple button" />
 			<GiftedChat
 				renderDay={renderDay}
 				messages={messages}
@@ -248,8 +312,8 @@ export function Chat(props) {
 				onInputTextChanged={setText}
 				onSend={onSend}
 				user={{
-					_id: 1,
-					name: 'Aaron',
+					_id: uid,
+					name: name,
 					avatar: 'https://placeimg.com/150/150/any',
 				}}
 				renderBubble={renderBubble} //renderBubble prop, looked up props in documentation
